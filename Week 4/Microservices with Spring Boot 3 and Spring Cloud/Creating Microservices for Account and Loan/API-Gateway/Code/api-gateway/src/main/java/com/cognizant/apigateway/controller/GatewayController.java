5fc8ced56f4d20bda1f6f187dd4b79514e255e2b
Harshini-chiramani <230841.cb@rmkec.ac.in>
2026-07-16 14:31:13 +0530

    Completed API Gateway

---
+++ Week 4/Microservices with Spring Boot 3 and Spring Cloud/Creating Microservices for Account and Loan/API-Gateway/Code/api-gateway/src/main/java/com/cognizant/apigateway/controller/GatewayController.java
@@ -0,0 +1,13 @@
+package com.cognizant.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API Gateway is running successfully!";
+    }
+}
